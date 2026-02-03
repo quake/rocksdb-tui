c7bff4d0c02f9d4a238187dee5a548e8eb4c830b
quake <quake.wang@gmail.com>
2026-02-03 20:05:49 +0900

    feat: update config to support key_schema and key_schema_file

--- src/config.rs
+++ src/config.rs
@@ -11,8 +11,10 @@ pub struct Config {
 #[derive(Debug, Clone, Deserialize)]
 pub struct ColumnFamilyConfig {
     pub name: String,
-    #[serde(default = "default_key_format")]
-    pub key_format: KeyFormat,
+    // Key format: inline schema, preset, or file reference
+    pub key_schema: Option<String>,
+    pub key_schema_file: Option<String>,
+    // Value format
     #[serde(default = "default_value_format")]
     pub value_format: ValueFormat,
     // Protobuf config
@@ -27,16 +29,6 @@ pub struct ColumnFamilyConfig {
 
 #[derive(Debug, Clone, Deserialize, Default)]
 #[serde(rename_all = "snake_case")]
-pub enum KeyFormat {
-    String,
-    #[default]
-    Hex,
-    U64Be,
-    U64Le,
-}
-
-#[derive(Debug, Clone, Deserialize, Default)]
-#[serde(rename_all = "snake_case")]
 pub enum ValueFormat {
     String,
     #[default]
@@ -45,10 +37,6 @@ pub enum ValueFormat {
     Msgpack,
     Protobuf,
     Molecule,
-}
-
-fn default_key_format() -> KeyFormat {
-    KeyFormat::Hex
 }
 
 fn default_value_format() -> ValueFormat {
@@ -66,5 +54,57 @@ impl Config {
 
     pub fn get_cf_config(&self, name: &str) -> Option<&ColumnFamilyConfig> {
         self.column_families.iter().find(|cf| cf.name == name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_config_with_key_schema() {
+        let toml = r#"
+[[column_families]]
+name = "blocks"
+key_schema = """
+seq:
+  - id: block_num
+    type: u8le
+"""
+value_format = "json"
+"#;
+        let config: Config = toml::from_str(toml).unwrap();
+        assert_eq!(config.column_families.len(), 1);
+        assert!(config.column_families[0].key_schema.is_some());
+    }
+
+    #[test]
+    fn test_parse_config_with_preset() {
+        let toml = r#"
+[[column_families]]
+name = "simple"
+key_schema = "hex"
+value_format = "string"
+"#;
+        let config: Config = toml::from_str(toml).unwrap();
+        assert_eq!(
+            config.column_families[0].key_schema,
+            Some("hex".to_string())
+        );
+    }
+
+    #[test]
+    fn test_parse_config_with_schema_file() {
+        let toml = r#"
+[[column_families]]
+name = "accounts"
+key_schema_file = "schemas/account_key.ksy"
+value_format = "json"
+"#;
+        let config: Config = toml::from_str(toml).unwrap();
+        assert_eq!(
+            config.column_families[0].key_schema_file,
+            Some("schemas/account_key.ksy".to_string())
+        );
     }
 }
